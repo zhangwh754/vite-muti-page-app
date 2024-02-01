1e52da83fff97e73241f20bb17ff90d482fb75b4
张文皓 <zhangwh754@163.com>
2024-02-01 10:19:31 +0800

    feat: 打包

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,14 @@ export default defineConfig({
   root: "./src/pages",
 
   build: {
+    outDir: resolve(process.cwd(), "build"), // 指定输出路径（相对于 项目根目录)
     rollupOptions: {
       input: {
         home: resolve(__dirname, "./src/pages/home/index.html"),
         banner: resolve(__dirname, "./src/pages/banner/index.html"),
+      },
+      output: {
+        esModule: false,
       },
     },
   },
