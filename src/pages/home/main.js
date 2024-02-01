475a85121a674616bda5c05f5c30db69a080d6c5
张文皓 <zhangwh754@163.com>
2024-02-01 23:19:21 +0800

    feat: 接口请求

--- src/pages/home/main.js
+++ src/pages/home/main.js
@@ -8,4 +8,22 @@ $(() => {
   $('button').on('click', _aaa => {
     window.location.href = `../banner/index.html`
   })
+
+  $.ajax('https://v2.api-m.com/api/weibohot').then(res => {
+    const list = $('.list')
+
+    JSON.parse(res)
+      .data.map(item => ({
+        title: item.title,
+        hot: item.hot,
+        url: item.url,
+      }))
+      .forEach(item => {
+        const listItem = $('<li></li>')
+        const link = $('<a></a>').attr('href', item.url).attr('target', '_blank').text(item.title)
+        const num = $('<span></span>').text(item.hot)
+        listItem.append(link).append(num)
+        list.append(listItem)
+      })
+  })
 })
