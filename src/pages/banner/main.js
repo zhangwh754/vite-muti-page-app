e258caee34285f9aad8a813d343c17c2de40da6f
张文皓 <zhangwh754@163.com>
2024-02-02 09:40:36 +0800

    feat: swiper引入

--- src/pages/banner/main.js
+++ src/pages/banner/main.js
@@ -1,7 +1,32 @@
+import Swiper from 'swiper/bundle'
+import 'swiper/css/bundle'
+
 $(() => {
   console.log('banner')
 
   $('button').on('click', () => {
     window.location.href = `../home/index.html`
+  })
+
+  new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   })
 })
